5725e9fef0f0265c27ed90fe7219b1c9ad350483
bohacek <stephan.bohacek@gmail.com>
2019-11-10 15:50:18 -0500

    Update publisher.py

--- IntroToCloudComputing/pubsub/publisher.py
+++ IntroToCloudComputing/pubsub/publisher.py
@@ -3,11 +3,13 @@ import os
 # or, go to settings and add package google-cloud-pubsub
 from google.cloud import pubsub_v1
 
+# you need to set the location of GCP credentials json file
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="D:/gcpFun/mycreds.json"
 
+# you need to set your project id
 PROJECT_ID = 'qwiklabs-gcp-00-b5fcde6c3cb0'
 
-TOPIC_NAME = 'myTopic'  # Set this to something appropriate.
+TOPIC_NAME = 'myTopic' # Set this to something, but be sure that it matches the topic name used in the subscriber code.
 
 topic_full_name = 'projects/{project_id}/topics/{topic}'.format(
     project_id=PROJECT_ID,
